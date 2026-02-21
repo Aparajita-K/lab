23a02d0d6d81fd0b434f2794e24d82428ded0681
Aparajita Khan <aparajitakhan1107@gmail.com>
2026-02-21 04:00:00 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
-import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
-import icon from "astro-icon";
-import lit from "@astrojs/lit";
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.sapling.supply/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  sitemap: true, // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
-});
+  site: 'https://aparajita-k.github.io',
+})
