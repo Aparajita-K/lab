598e5abbef8b47a9a26c9cd60473a0643806f4e8
Aparajita Khan <aparajitakhan1107@gmail.com>
2026-02-21 15:39:44 +0530

    Update nav.js

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,18 +1,11 @@
 export const nav = [
-	{
-		title: 'Home',
-		slug: '/',
-	},
-	{
-		title: 'Blog',
-		slug: '/blog',
-	},
-	{
-		title: 'About',
-		slug: '/company/about',
-	},
-	{
-		title: 'Contact',
-		slug: '/company/contact',
-	},
+  { title: "Home", slug: "/" },
+  { title: "Research", slug: "/research" },
+  { title: "Team", slug: "/team" },
+  { title: "Academics", slug: "/academics" },
+  { title: "Publications", slug: "/publications" },
+  { title: "Resources", slug: "/resources" },
+  { title: "News", slug: "/news" },
+  { title: "Collaborators", slug: "/collaborators" },
+  { title: "Contact", slug: "/contact" },
 ];
